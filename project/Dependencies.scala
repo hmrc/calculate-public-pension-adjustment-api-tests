18b12330adaffc22cafaaddbe13bcfd74132433a
lahiru.wijesinghe@opencastsoftware.com <lahiru.wijesinghe@opencastsoftware.com>
2023-06-23 15:59:16 +0100

    MCSC-176 scripting

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,6 +8,9 @@ object Dependencies {
     "com.typesafe"         % "config"                  % "1.3.2"   % Test,
     "com.typesafe.play"   %% "play-ahc-ws-standalone"  % "2.1.2"   % Test,
     "org.slf4j"            % "slf4j-simple"            % "1.7.25"  % Test,
-    "com.typesafe.play"   %% "play-ws-standalone-json" % "2.1.2"   % Test
+    "com.typesafe.play"   %% "play-ws-standalone-json" % "2.1.2"   % Test,
+    "org.scalatest"       %% "scalatest"               % "3.2.9"   % Test,
+    "io.circe"            %% "circe-core"              % "0.14.1"  % Test,
+    "io.circe"            %% "circe-parser"            % "0.14.1"  % Test
   )
 }
--- src/test/scala/uk/gov/hmrc/test/api/specs/CalculatePublicPensionAdjustmentSpec.scala
+++ src/test/scala/uk/gov/hmrc/test/api/specs/CalculatePublicPensionAdjustmentSpec.scala
@@ -17,26 +17,41 @@
 package uk.gov.hmrc.test.api.specs
 
 import play.api.libs.ws.StandaloneWSResponse
-import uk.gov.hmrc.test.api.utils.ApiLogger
+import uk.gov.hmrc.test.api.utils.JsonUtils.{convertJsonFormatASString, getResponseJsonFileAsString}
 
 class CalculatePublicPensionAdjustmentSpec extends BaseSpec {
   val uri = "submit-public-pension-adjustment/calculation/submit"
 
   Feature("Calculate Public Pension Adjustment API functionality") {
-    Scenario("Verify Calculate Public Pension Adjustment API works fine by using direct json") {
+    val requestArray: Array[String]  =
+      Array("request10", "request11", "request12", "request14", "request16", "request17")
+    val responseArray: Array[String] =
+      Array("response10", "response11", "response12", "response14", "response16", "response17")
+    requestArray.indices.foreach { index =>
+      Scenario(s"Verify Calculate Public Pension Adjustment API works fine - Test data set $index") {
+        Given("I got a valid url and test file name")
+        val uri              = "show-calculation"
+        val requestFileName  = requestArray(index)
+        val responseFileName = responseArray(index)
 
-      Given("I got a valid url and test file name")
-      val uri      = "show-calculation"
-      val fileName = "valid"
+        When("I use the calculate public pension adjustment API request to get a valid response")
+        val response: StandaloneWSResponse =
+          calculatePublicPensionAdjustmentHelper.calculatePostRequest(uri, requestFileName, "123")
 
-      When("I use the calculate public pension adjustment API request to get a valid response")
-      val response: StandaloneWSResponse =
-        calculatePublicPensionAdjustmentHelper.calculatePostRequest(uri, fileName, "123")
+        Then("I got the status code 200")
+        response.status shouldBe 200
 
-      ApiLogger.log.info("Status : " + response.status)
-      ApiLogger.log.info("Body : " + response.body)
-      Then("I got the status code 200")
-      response.status shouldBe 200
+        When("I get the response body as a string")
+        val jsonBody         = response.body.mkString
+        val jsonResponseBody = convertJsonFormatASString(jsonBody)
+
+        When("I get the test data response file as a string")
+        val jsonResponseFile = convertJsonFormatASString(getResponseJsonFileAsString(responseFileName))
+
+        Then("I verify the response exactly matching with the test data response file")
+        assert(jsonResponseBody == jsonResponseFile, "Response is not as expected")
+
+      }
     }
   }
 }
